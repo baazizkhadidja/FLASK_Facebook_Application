ec2aed8d7f4b55e742355d3c43b43b8fde934f82
BAAZIZ KHADIDJA <baazize-khadija@hotmail.com>
2021-01-15 13:54:32 +0100

    version de mon code avec l integration d une base de données

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
 FB_APP_ID = 405030733919306
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
